fd4bb232cbbe237f37617dfcec1e14efcd8890ca
O Morrill <owenpm3@1644952.no-reply.drupal.org>
2017-04-17 14:18:37 -0600

    Initial commit.

---
+++ src/Form/LayoutConfigSettingsForm.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\layout_config\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\block\Controller\BlockLibraryController;
+
+/**
+* Configure example settings for this site.
+*/
+class LayoutConfigSettingsForm extends ConfigFormBase {
+
+  /**
+  * {@inheritdoc}
+  */
+  public function getFormId() {
+    return 'example_admin_settings';
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  protected function getEditableConfigNames() {
+    return [
+      'block.block.searchform_2',
+    ];
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('block.block.searchform_2');
+
+    $form['example_thing'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Things'),
+      '#default_value' => $config->get('things'),
+    );
+
+    $form['region'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Other things'),
+      '#default_value' => $config->get('region'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration
+    $this->config('block.block.searchform_2')
+    // Set the submitted configuration setting
+    ->set('things', $form_state->getValue('example_thing'))
+    // You can set multiple configurations at once by making
+    // multiple calls to set()
+    ->set('region', $form_state->getValue('region'))
+    ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
+
+/*
+
+a:12:{
+  s:4:"uuid";s:36:"6f5441e3-93b5-47d1-9792-c1f9816b8a53";
+  s:8:"langcode";s:2:"en";
+  s:6:"status";b:1;
+  s:12:"dependencies";a:2:{s:6:"module";a:2:{i:0;s:6:"search";i:1;s:6:"system";}
+  s:5:"theme";a:1:{i:0;s:14:"fiddlers_green";}}
+  s:2:"id";
+  s:12:"searchform_2";
+  s:5:"theme";s:14:"fiddlers_green";
+  s:6:"region";s:13:"sidebar_first";
+  s:6:"weight";i:0;
+  s:8:"provider";N;
+  s:6:"plugin";s:17:"search_form_block";
+  s:8:"settings";a:4:{s:2:"id";s:17:"search_form_block";s:5:"label";s:11:"Search form";s:8:"provider";s:6:"search";s:13:"label_display";s:7:"visible";}
+  s:10:"visibility";a:1:{s:12:"request_path";a:4:{s:2:"id";s:12:"request_path";s:5:"pages";s:7:"/owen/*";s:6:"negate";b:0;s:15:"context_mapping";a:0:{}}}
+}
+
+*/
